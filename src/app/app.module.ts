2c964206b56224028c06a72edac16d4ab88e8f83
MinhQuyet14 <minhquyet1401@gmail.com>
2024-03-23 00:05:57 +0700

    cm3 login_register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
 import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
@@ -16,7 +19,10 @@ import { OrderComponent } from './order/order.component';
             HeaderComponent,
             FooterComponent,
             DetailProductComponent,
-            OrderComponent
+            OrderComponent,
+            OrderConfirmComponent,
+            LoginComponent,
+            RegisterComponent
   ],
   imports: [
     BrowserModule
@@ -25,7 +31,10 @@ import { OrderComponent } from './order/order.component';
   bootstrap: [
     //HomeComponent,
     //DetailProductComponent,
-    OrderComponent
+    //OrderComponent,
+    //OrderConfirmComponent,
+    //LoginComponent,
+    RegisterComponent
   ]
 })
 export class AppModule { }
