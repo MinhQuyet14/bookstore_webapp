b9e72374801ae7fda21034308f7a4131cdbb6c8a
MinhQuyet14 <minhquyet1401@gmail.com>
2024-04-18 20:43:19 +0700

    no cm

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('registerForm') registerForm!: NgForm;
+  phone: string;
+  password: string;
+  retypePassword: string;
+  fullName: string;
+  address: string;
+  constructor() {
+    this.phone = '';
+    this.password = '';
+    this.retypePassword = '';
+    this.fullName = '';
+    this.address = '';
+   }
 
-  constructor() { }
+   onPhoneChange(){
+    console.log(`Phone typed:  ${this.phone}`)
+   }
 
   ngOnInit(): void {
   }
+  register(){
+    const message = `phone: ${this.phone}` + 
+                    `password: ${this.password}` +
+                    `retypePassword: ${this.retypePassword}` + 
+                    `fullName: ${this.fullName}` +
+                    `address: ${this.address}`;
 
+    alert(message);
+  }
+  checkPasswordsMatch(){
+    if(this.password !== this.retypePassword){
+      this.registerForm.form.controls['retypePassword'].setErrors({'passwordMissmatch': true});
+    }else {
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
 }
